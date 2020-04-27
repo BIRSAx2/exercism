93bcfb997a674afaf02d4034ba6b573282efcd20
BIRSAx2 <mouhieddinesabir@gmail.com>
2020-04-27 12:01:00 +0200

    added the solution for "pangram"

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = (sentence) => {
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+  let letter;
+  if (sentence.length==0) 
+    return false
+  console.log(sentence)
+  letters.split("").forEach(element => {
+     if(!sentence.includes(element))
+      return false;
+  });
+  return true;
 };
