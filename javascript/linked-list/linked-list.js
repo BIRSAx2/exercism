8d5c9cabedbd68f5342be051104c50af5d0f83ea
BIRSAx2 <mouhieddinesabir@gmail.com>
2020-05-10 23:13:55 +0200

    Added linked-list solution

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -3,28 +3,87 @@
 // convenience to get you started writing code faster.
 //
 
+const Node = function(value){
+  this.value=value;
+  this.next=null;
+  this.previous=null;
+  return this.value;
+};
+
 export class LinkedList {
-  push() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(){
+    this.length=0;
+    this.tail=null;
+    this.head=null;
   }
+  push(value) {
+    const node= new Node(value)
+    if(this.length===0){
+      this.tail=node;
+      this.head=node;
+    } else{
+      this.tail.next=node;
+      node.previous=this.tail;
+      this.tail=node;
+    }
+    this.length++;
+    return node;
+  } 
 
   pop() {
-    throw new Error("Remove this statement and implement this function");
+    const node= this.delete(this.tail);
+    if(this.tail)
+      this.tail.next=null;
+    return node.value
   }
 
   shift() {
-    throw new Error("Remove this statement and implement this function");
+    const node= this.delete(this.head);
+    this.head=node.next;
+    return node.value;
   }
 
-  unshift() {
-    throw new Error("Remove this statement and implement this function");
+  unshift(value) {
+    const node = new Node(value);
+    if(this.length===0){
+      this.head=node;
+      this.tail=node;
+    } else{
+      const prevHead = this.head;
+      this.head.previous=node;
+      this.head=node;
+      this.head.next=prevHead;
+    }
+    this.length++;
+    return node.value
   }
 
-  delete() {
-    throw new Error("Remove this statement and implement this function");
+  delete(value) {
+    const valueToSearch = value instanceof Node ? value.value : value;
+    let searchNode=this.head;
+    let foundNode=null;
+    if(valueToSearch===this.head.value){
+      foundNode=this.head;
+      this.head=this.head.next;
+    }
+    if(valueToSearch===this.tail.value){
+      foundNode=this.tail;
+      this.tail=this.tail.previous;
+    }
+    while(!searchNode || !foundNode){
+      if(valueToSearch===searchNode.value){
+        foundNode=searchNode;
+        foundNode.previous=foundNode.next;
+      }else{
+        searchNode=this.head.next;
+      }      
+    }
+    if(foundNode)
+      this.length--;
+    return foundNode;
   }
 
   count() {
-    throw new Error("Remove this statement and implement this function");
+    return this.length;
   }
 }
