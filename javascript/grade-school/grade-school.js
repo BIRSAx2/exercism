53dfb227943038912bb5b16ff265a445f8a0f0d2
BIRSAx2 <mouhieddinesabir@gmail.com>
2020-05-10 23:30:31 +0200

    added grade-school solution

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Grade School' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class GradeSchool {
+  constructor(){
+    this._roster={};
+  }
+  roster() {
+    return this._roster;
+  }
+
+  add(studentName,year) {
+    if(!this._roster[year])
+      this._roster[year]=[]
+    this._roster[year].push(studentName);
+    this._roster[year].sort();
+  }
+
+  grade(year) {
+    return (!this._roster[year]) ? [] : this._roster[year];
+  }
+}
