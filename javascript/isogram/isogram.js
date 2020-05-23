a5d6a60cd9fb986ddd55123ae670f1db43465323
BIRSAx2 <mouhieddinesabir@gmail.com>
2020-05-23 14:07:14 +0200

    Added solution for isogram.js

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isIsogram = (string) => {
+  return !/(\w).*\1/i.test(string)
+};
